65c4603050d61ba4f30bcbf1fd25ee2f244c67c7
David Weik <geekupyourlife@gmail.com>
2016-03-07 11:23:49 +0100

    Entfernen Android Standardmenü
    In der LoginActivity.java

--- app/src/main/java/de/dhbw/pizzabutler/LoginActivity.java
+++ app/src/main/java/de/dhbw/pizzabutler/LoginActivity.java
@@ -6,7 +6,6 @@ import android.content.SharedPreferences;
 import android.content.res.TypedArray;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -41,22 +40,6 @@ public class LoginActivity extends BaseActivity {
         //Übergabe der Eingabefelder in Variablen
         eingabeUser = (EditText) findViewById(R.id.benutzername);
         eingabePasswort = (EditText) findViewById(R.id.passwort_login);
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        //if (id == R.id.action_settings) {
-            //return true;
-        //}
-
-        return super.onOptionsItemSelected(item);
     }
 
     /**

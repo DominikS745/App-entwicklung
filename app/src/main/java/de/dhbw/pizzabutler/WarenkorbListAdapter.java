02bb99f91cce075c2a7efaa72d90f208d6225218
Marvin Messenzehl <marvin.messenzehl@gmx.de>
2016-04-04 10:24:48 +0200

    Überarbeiten des Adapters für Warenkorb Adapter --> WarenkorbListAdapter (Noch Empty vom Inhalt her)

---
+++ app/src/main/java/de/dhbw/pizzabutler/WarenkorbListAdapter.java
@@ -0,0 +1,57 @@
+package de.dhbw.pizzabutler;
+
+/**
+ * Created by Marvin on 04.03.16.
+ */
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.springframework.util.support.Base64;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import de.dhbw.pizzabutler_entities.Pizzeria;
+
+public class WarenkorbListAdapter extends ArrayAdapter<String> {
+
+    //Wichtig --> Irgendeine Entity finden oder anlegen, in der alle benötigten Daten stehen!
+    Context context;
+    private static LayoutInflater inflater = null;
+
+    public WarenkorbListAdapter(Context context, ArrayList<String> data) {
+        super(context, 0, data);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        //Get Item Data for this position
+        String data = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.pizzaria_liste_item, parent, false);
+        }
+        // Lookup view for data population
+
+
+        // Populate the data into the template view using the data object
+
+
+        // Return the completed view to render on screen
+        return convertView;
+
+    }
+
+
+}
